c5717006a48a397f3a1631bbf24fbb9466768d54
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 15:07:29 +0530

    add the private data members

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
@@ -1,4 +1,11 @@
 package com.railway.scheduling.Data;
 
 public class Train {
+    private int id;
+    private String type;
+    private int speed;
+    private int priority;
+    private int currentLocation;
+    private int arraivalTime;
+    private int departureTime;
 }
