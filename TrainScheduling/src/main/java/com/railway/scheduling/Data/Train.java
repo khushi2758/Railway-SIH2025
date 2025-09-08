309808e532c1f87da186cf2d0d007af8df04cb3d
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 15:18:29 +0530

    add the no argument constructor to make a constructor for empty parameters

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
@@ -2,9 +2,11 @@ package com.railway.scheduling.Data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data //add the data annotation from lombok to generate the getter, setter, toString(), equals(), hashCode() methods for all the attributes
 @AllArgsConstructor //add the all argument constructor to make a constructor for every attribute
+@NoArgsConstructor //add the no argument constructor to make a constructor for empty parameters
 public class Train {
     private int id;
     private String type;
