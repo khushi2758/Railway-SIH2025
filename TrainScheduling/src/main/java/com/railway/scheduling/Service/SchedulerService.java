5567a54666f7d1ba5af4347518de5d6dc93a94c6
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 15:57:33 +0530

    add a method name schedule trains which take List of trains and list of platform as input and return integer as output

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
@@ -1,7 +1,14 @@
 package com.railway.scheduling.Service;
 
+import com.railway.scheduling.Data.Platform;
+import com.railway.scheduling.Data.Train;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SchedulerService {
+    public int scheduleTrains(List<Train> trains, List<Platform> platforms){
+
+    }
 }
