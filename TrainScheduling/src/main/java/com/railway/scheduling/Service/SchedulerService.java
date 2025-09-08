e16681a881f26d397482700dc8a5742cd64dc832
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 16:16:44 +0530

    create a hashmap for state and integer

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
@@ -5,9 +5,7 @@ import com.railway.scheduling.Data.State;
 import com.railway.scheduling.Data.Train;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 @Service
 public class SchedulerService {
@@ -15,5 +13,6 @@ public class SchedulerService {
         int numberOfTrains = trains.size();
         int platformCount = platforms.size();
         PriorityQueue<State> queue = new PriorityQueue<>((s1,s2)-> Integer.compare(s1.getTotalDelay(),s2.getTotalDelay()));
+        Map<State, Integer> dp = new HashMap<>();
     }
 }
