137443f43782faa93b426acdffd66decca0264e7
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 15:25:42 +0530

    add the all argument constructor to make a constructor for every attribute

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Platform.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Platform.java
@@ -1,8 +1,10 @@
 package com.railway.scheduling.Data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Platform {
     private int id;
     private int trackAvailability; // 0 = empty, 1 = occupied
