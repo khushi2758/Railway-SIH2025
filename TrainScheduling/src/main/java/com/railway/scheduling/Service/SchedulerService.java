264ba748e5a1dc6917eca4e207784d3cb89ca762
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 16:11:28 +0530

    create a priority queue for state

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
@@ -1,15 +1,19 @@
 package com.railway.scheduling.Service;
 
 import com.railway.scheduling.Data.Platform;
+import com.railway.scheduling.Data.State;
 import com.railway.scheduling.Data.Train;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 @Service
 public class SchedulerService {
     public int scheduleTrains(List<Train> trains, List<Platform> platforms){
         int numberOfTrains = trains.size();
         int platformCount = platforms.size();
+        PriorityQueue<State> queue = new PriorityQueue<>((s1,s2)-> Integer.compare(s1.getTotalDelay(),s2.getTotalDelay()));
     }
 }
