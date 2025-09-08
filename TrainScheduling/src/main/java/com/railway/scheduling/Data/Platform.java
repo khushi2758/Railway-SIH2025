50a327482972746845f11b2aa8964b24fbfeda2f
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 15:24:43 +0530

    add the private data members

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Platform.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Platform.java
@@ -1,4 +1,8 @@
 package com.railway.scheduling.Data;
 
 public class Platform {
+    private int id;
+    private int trackAvailability; // 0 = empty, 1 = occupied
+    private int trackHealth; // percentage
+    private int availableFromTime; //in minute
 }
