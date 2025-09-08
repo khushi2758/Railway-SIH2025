00e6620c062e65e46555f946016100d0595ea63d
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 15:17:42 +0530

    add the all argument constructor to make a constructor for every attribute

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
@@ -1,9 +1,10 @@
 package com.railway.scheduling.Data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data //add the data annotation from lombok to generate the getter, setter, toString(), equals(), hashCode() methods for all the attributes
-
+@AllArgsConstructor //add the all argument constructor to make a constructor for every attribute
 public class Train {
     private int id;
     private String type;
