341af2bcb8c4ac1c6177b155cda3df2f976e9757
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-08 15:31:22 +0530

    add the no argument constructor to make a constructor for empty parameters

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/State.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/State.java
@@ -2,9 +2,11 @@ package com.railway.scheduling.Data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class State {
     private int[] platformAvailableFrom;
     private int scheduleMask;
