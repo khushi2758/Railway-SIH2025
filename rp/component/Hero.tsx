d8751e470bf9ddae3cfef735115b01a10a18d411
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-09-08 12:14:53 +0530

    dashboard

--- rp/component/Hero.tsx
+++ rp/component/Hero.tsx
@@ -1,44 +1,38 @@
-import React from 'react'
-import { cn } from "@/lib/utils";
-import { Spotlight } from './ui/Spotlight';
-import { BoxesCore } from './ui/background-boxes';
-
+import React from "react";
+import { NavbarDemo } from "./Navbar";
+import { Card } from "./Card";
+import { BoxesCore } from "./ui/background-boxes";
 
 const Hero = () => {
-
   return (
-    <div className='pb-20 pt-36 '>
-      <div>
-             <Spotlight gradientFirst ="radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .06) 0, hsla(210, 89%, 41%, 0.60) 80%, transparent 100%)" translateY={-200} width={800} height={800} duration={10} xOffset={200}/>
-             <Spotlight gradientSecond ="radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .04) 0, hsla(240, 100%, 45%, 0.02) 80%, transparent 100%)" translateY={-100} width={600} height={600} duration={8} xOffset={150}/>
-      </div>
-      <div className="absolute flex h-screen w-full items-center  bg-white dark:bg-black  item-center justify-center top-0 left-0">
-             <div
-               className={cn(
-                 "absolute inset-0",
-                 "[background-size:40px_40px]",
-                 "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-                 "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-               )}
-             />
-             {/* Radial gradient for the container to give a faded look */}
-             <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]
-              dark:bg-black"/>
-      </div>
-
-      <div className="flex justify-center relative my-20 text-amber-50 z-10">
-        <BoxesCore/>
+    <>
+      <div className="mt-5">
+        <div className="absolute flex h-screen w-full items-center  bg-white dark:bg-black  item-center justify-center top-0 left-0">
+          <BoxesCore />
+          {/* Radial gradient for the container to give a faded look */}
+          <div
+            className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]
+              dark:bg-black"
+          />
         </div>
-       
+        <NavbarDemo />
+        <div className="flex justify-center relative my-20 text-amber-50 z-10">
+          <div className="max-w-[89vw] flex flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 mb-6">
+              RAILTRACK HEALTH MONITOR SYATEM
+            </h2>
+            <div className="flex flex-row-reverse gap-7  items-center justify-center-safe">
+              <Card  title="Structural" persentage={80}/>
+              <Card title= "Rail" persentage={90}/>
+              <Card title="Sleeper" persentage={70}/>
+              <Card title= "Ballast"persentage={95}/>
+              <Card title="Environmental" persentage={99} />
+            </div>
+          </div>
         </div>
+      </div>
+    </>
+  );
+};
 
-    
-
-   
-      
-  
-  
-  )
-}
-
-export default Hero
+export default Hero;
