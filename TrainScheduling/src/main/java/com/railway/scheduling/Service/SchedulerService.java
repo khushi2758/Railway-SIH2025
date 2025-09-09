6779da72e95455df2155d3801c8a83c048d2f27c
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-10 00:14:22 +0530

    store the numbers of trains and number of platform in local variable

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
@@ -10,18 +10,8 @@ import java.util.*;
 @Service
 public class SchedulerService {
     public int scheduleTrains(List<Train> trains, List<Platform> platforms){
-        int numberOfTrains = trains.size();
-        int platformCount = platforms.size();
-        PriorityQueue<State> queue = new PriorityQueue<>((s1,s2)-> Integer.compare(s1.getTotalDelay(),s2.getTotalDelay()));
-        Map<State, Integer> dp = new HashMap<>();
-//      Initial state: all platform free, no trains scheduled
-        int[] initialAvailable = new int[platformCount];
-        Arrays.fill(initialAvailable,0);
-        State initialState = new State(initialAvailable,0,0);
-        queue.add(initialState);
-        dp.put(initialState,0);
-        while(!queue.isEmpty()){
-            State current = queue.poll();
-        }
+        int numbersOfTrains = trains.size();
+        int numberOfPlatform = platforms.size();
+
     }
 }
