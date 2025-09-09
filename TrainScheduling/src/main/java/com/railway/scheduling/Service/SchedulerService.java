7720e4c3ad47099ccfe679053db30763434b6806
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-10 00:30:20 +0530

    complete a function that check every possible combination and store it

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/SchedulerService.java
@@ -5,10 +5,34 @@ import com.railway.scheduling.Data.State;
 import com.railway.scheduling.Data.Train;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 @Service
 public class SchedulerService {
+    Map<String,Integer> memo = new HashMap<>();
+    public int dp(List<Train>trains,int[]platformTimes,int trainIndex){
+        if(trainIndex==trains.size()){
+            return 0;
+        }
+        String key = trainIndex+Arrays.toString(platformTimes);
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        Train train = trains.get(trainIndex);
+        int minDelay = Integer.MAX_VALUE;
+        for(int p=0;p<platformTimes.length;p++){
+            int availableTime = platformTimes[p];
+            int startTime = Math.max(availableTime,train.getArrivalTime());
+            int waitTime = startTime - train.getArrivalTime();
+            int[] newPlatFormTimes = Arrays.copyOf(platformTimes,platformTimes.length);
+            newPlatFormTimes[p] = startTime+train.getDwellTime();
+            int totalDelay = waitTime + dp(trains,newPlatFormTimes,trainIndex);
+            minDelay = Math.min(minDelay,totalDelay);
+        }
+        memo.put(key,minDelay);
+        return minDelay;
+    }
     public int scheduleTrains(List<Train> trains, List<Platform> platforms){
         int numbersOfTrains = trains.size();
         int numberOfPlatform = platforms.size();
