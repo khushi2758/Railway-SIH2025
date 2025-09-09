b1d6013fe33bf91161a79708bd2e5372d6ea3344
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-10 00:42:18 +0530

    add the instance of service class and autowired it

--- TrainScheduling/src/main/java/com/railway/scheduling/Controller/ScheduleController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Controller/ScheduleController.java
@@ -1,9 +1,13 @@
 package com.railway.scheduling.Controller;
 
+import com.railway.scheduling.Service.SchedulerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/platform")
 public class ScheduleController {
+    @Autowired
+    private SchedulerService schedulerService;
 }
