636cf914b1d4b4beb2c9f7771dad142eedaf91f0
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-10 00:11:39 +0530

    remove the track health factor

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Platform.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Platform.java
@@ -10,6 +10,5 @@ import lombok.NoArgsConstructor;
 public class Platform {
     private int id;
     private int trackAvailability; // 0 = empty, 1 = occupied
-    private int trackHealth; // percentage
     private int availableFromTime; //in minute
 }
