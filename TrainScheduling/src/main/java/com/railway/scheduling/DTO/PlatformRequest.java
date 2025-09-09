1e2f5972c4cc0e507df984d71246bcff722b80cd
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-10 00:40:21 +0530

    add the private data member and the useful annotation

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/PlatformRequest.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/PlatformRequest.java
@@ -1,4 +1,16 @@
 package com.railway.scheduling.DTO;
 
+import com.railway.scheduling.Data.Train;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlatformRequest {
+    private List<Train> trains;
+    private int numPlatforms;
 }
