f59edf7f421c61101369371004de0d893c618609
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-10 00:43:25 +0530

    added the post mapping and create a method of assign platform

--- TrainScheduling/src/main/java/com/railway/scheduling/Controller/ScheduleController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Controller/ScheduleController.java
@@ -1,7 +1,10 @@
 package com.railway.scheduling.Controller;
 
+import com.railway.scheduling.DTO.PlatformRequest;
 import com.railway.scheduling.Service.SchedulerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleController {
     @Autowired
     private SchedulerService schedulerService;
+    @PostMapping("/assign")
+    public int assignPlatforms(@RequestBody PlatformRequest platformRequest){
+
+    }
 }
