5206e929a8d683c0eb5f3fa7fc1a7c0695f11191
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-11 10:34:51 +0530

    change the return type from integer to response entity and store the total delay in temporary variable

--- TrainScheduling/src/main/java/com/railway/scheduling/Controller/ScheduleController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Controller/ScheduleController.java
@@ -3,10 +3,13 @@ package com.railway.scheduling.Controller;
 import com.railway.scheduling.DTO.PlatformRequest;
 import com.railway.scheduling.Service.SchedulerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/platform")
@@ -14,7 +17,7 @@ public class ScheduleController {
     @Autowired
     private SchedulerService schedulerService;
     @PostMapping("/assign")
-    public int assignPlatforms(@RequestBody PlatformRequest platformRequest){
-        return schedulerService.scheduleTrains(platformRequest.getTrains(),platformRequest.getNumPlatforms());
+    public ResponseEntity<Map<String,String>> assignPlatforms(@RequestBody PlatformRequest platformRequest){
+        int res = schedulerService.scheduleTrains(platformRequest.getTrains(),platformRequest.getNumPlatforms());
     }
 }
