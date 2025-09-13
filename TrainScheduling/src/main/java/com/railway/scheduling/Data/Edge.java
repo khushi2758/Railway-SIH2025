386a86885ae1162593d5d85df3754f181a0322ee
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-13 14:59:40 +0530

    add the private data members and also create a constructor

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Edge.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Edge.java
@@ -1,4 +1,28 @@
 package com.railway.scheduling.Data;
 
 public class Edge {
+    int dist;
+    boolean freeByTrain;
+    double delayTime;
+    int trackHealthOk;
+    double speed;
+    double totalWeight;
+    double baseCost;
+    double penalty;
+    double trainSpecificCost;
+
+    public Edge(int dist, boolean freeByTrain, double delayTime, int trackHealthOk, double speed) {
+        this.dist = dist;
+        this.freeByTrain = freeByTrain;
+        this.delayTime = delayTime;
+        this.trackHealthOk = trackHealthOk;
+        this.speed = speed;
+        baseCost = dist/speed;
+        int k = 1;
+        penalty = k * delayTime;
+        double alpha = 1.0;
+        double bita = 1.0;
+        trainSpecificCost = alpha * baseCost + bita;
+        totalWeight = baseCost+penalty+trainSpecificCost;
+    }
 }
