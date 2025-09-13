470d8f6b395aa26ecc928d205863e4a69102ac5b
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-13 15:01:51 +0530

    add the no argument constructor adn data annotation

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Edge.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Edge.java
@@ -1,5 +1,10 @@
 package com.railway.scheduling.Data;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class Edge {
     int dist;
     boolean freeByTrain;
