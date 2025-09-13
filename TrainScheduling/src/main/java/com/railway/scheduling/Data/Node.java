cf868b9474b133c2eb32f52f8360ddb7633527a5
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-13 15:04:29 +0530

    add some annotations

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Node.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Node.java
@@ -1,5 +1,12 @@
 package com.railway.scheduling.Data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Node {
     private int node;
     private Edge edge;
