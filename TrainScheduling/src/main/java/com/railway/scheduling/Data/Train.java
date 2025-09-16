eebed967a3b2f010794793c213a8fcbb6c3c8ab7
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-16 19:53:36 +0530

    create the train class

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Train.java
@@ -1,20 +1,4 @@
 package com.railway.scheduling.Data;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data //add the data annotation from lombok to generate the getter, setter, toString(), equals(), hashCode() methods for all the attributes
-@AllArgsConstructor //add the all argument constructor to make a constructor for every attribute
-@NoArgsConstructor //add the no argument constructor to make a constructor for empty parameters
 public class Train {
-    private int id;
-    private String type;
-    private int speed;
-    private int priority;
-    private int currentLocation;
-    private int arrivalTime;
-    private int departureTime;
-    private int dwellTime;
-    private int blockOccupency;
 }
