2fb27c1a0e9f11f61c01c39209fcde81b8f64698
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-09-18 14:51:05 +0530

    final push dont touch proloy

--- rp/app/page.tsx
+++ rp/app/page.tsx
@@ -3,13 +3,11 @@ import About from "@/app/About/page";
 import Hero from "@/app/Landing/page"; 
 import { NavbarDemo } from "@/component/Navbar";
 import BottomNav from "@/component/ui/buttomNav";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
     <>
-    <ToastContainer position="top-right" autoClose={3000} theme="colored" />
+    
     <NavbarDemo />
     <main className="min-h-screen overflow-y-scroll">
       <Hero />
