6e7e0fd1960ffbaca069f448d92ca5d72df596c4
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-21 21:14:07 +0530

    create the private data member of departure time of the train

--- TrainScheduling/src/main/java/com/railway/scheduling/Entity/Train.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Entity/Train.java
@@ -3,8 +3,11 @@ package com.railway.scheduling.Entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.sql.Time;
+
 @Entity
 public class Train {
     @Id
     private Long trainId;
+    private Time departureTime;
 }
