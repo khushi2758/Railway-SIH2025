a64fcbb41aa0f80cbd55d6212e51092164738598
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-21 21:57:58 +0530

    add the data annotation for every getter and setter method for every data members

--- TrainScheduling/src/main/java/com/railway/scheduling/Entity/Track.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Entity/Track.java
@@ -2,8 +2,10 @@ package com.railway.scheduling.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Track {
     @Id
     private Long trackID;
