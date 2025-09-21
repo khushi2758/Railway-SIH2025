e4487982ba76ec12b8597544d0e98457cece9e2b
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-21 21:57:34 +0530

    add the data annotation for every getter and setter method for every data members

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/TrackStatus.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/TrackStatus.java
@@ -1,7 +1,10 @@
 package com.railway.scheduling.Data;
 
+import lombok.Data;
+
 import java.sql.Time;
 
+@Data
 public class TrackStatus {
     private boolean occupancy;
     private Time freeTrack;
