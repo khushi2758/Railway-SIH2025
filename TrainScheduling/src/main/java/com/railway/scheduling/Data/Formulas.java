9aebbdcd817f316a59731464e7300bcba2ae7b36
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-22 22:09:34 +0530

    return the resultant answer

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Formulas.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Formulas.java
@@ -3,6 +3,6 @@ package com.railway.scheduling.Data;
 public final class Formulas {
     private Formulas(){}
     public static float baseCost(float trackLength,float maximumSpeedAllowed){
-
+        return trackLength / maximumSpeedAllowed;
     }
 }
