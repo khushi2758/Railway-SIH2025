c3217010aa172bb6d1838521c8c7c53137b90325
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-22 17:58:35 +0530

    add the data annotation for create an all argument constructor

--- TrainScheduling/src/main/java/com/railway/scheduling/Entity/Track.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Entity/Track.java
@@ -2,10 +2,12 @@ package com.railway.scheduling.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class Track {
     @Id
     private Long trackID;
