dcbe8832f79ff6d85ddaa78f5ff6fc8b727214ab
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-22 17:58:45 +0530

    add the data annotation for create an all argument constructor

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/TrackStatus.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/TrackStatus.java
@@ -1,10 +1,12 @@
 package com.railway.scheduling.Data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.sql.Time;
 
 @Data
+@AllArgsConstructor
 public class TrackStatus {
     private boolean occupancy;
     private Time freeTrack;
