af2c701522aa5ced5a69e5736f84aeb27c6c880d
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-22 22:08:27 +0530

    create a new function that calculate the base cost by tacking required parameters

--- TrainScheduling/src/main/java/com/railway/scheduling/Data/Formulas.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Data/Formulas.java
@@ -2,4 +2,7 @@ package com.railway.scheduling.Data;
 
 public final class Formulas {
     private Formulas(){}
+    public static float baseCost(float trackLength,float maximumSpeedAllowed){
+
+    }
 }
