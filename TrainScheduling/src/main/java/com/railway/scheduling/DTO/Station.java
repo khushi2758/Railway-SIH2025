91f8cc8fccc1c266ccf39d2862f3be7ab5ffeba1
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-24 19:21:48 +0530

    add the parameter which is the total cost of (g+h) means actual value + heuristic value

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
@@ -3,6 +3,7 @@ package com.railway.scheduling.DTO;
 public class Station implements Comparable<Station>{
     private String name;
     private int g; //cost from start
+    private int f; // g+h
     @Override
     public int compareTo(Station o) {
         return 0;
