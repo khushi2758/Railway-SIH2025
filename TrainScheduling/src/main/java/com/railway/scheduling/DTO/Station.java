57b740129e3b7cdefb81d2b8c59e3e75a3c6d434
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-24 19:19:02 +0530

    implement the Comparable interface with Station

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
@@ -1,5 +1,10 @@
 package com.railway.scheduling.DTO;
 
-public class Station {
+public class Station implements Comparable<Station>{
     private String name;
+
+    @Override
+    public int compareTo(Station o) {
+        return 0;
+    }
 }
