dbd9ce403874f9f1e741dfb566e346a15558f910
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-24 19:22:23 +0530

    add the parent node in the class

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
@@ -4,6 +4,7 @@ public class Station implements Comparable<Station>{
     private String name;
     private int g; //cost from start
     private int f; // g+h
+    Station parent;
     @Override
     public int compareTo(Station o) {
         return 0;
