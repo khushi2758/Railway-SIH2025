c01f43820632e2be39c5cb77b169bc2228c4d720
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-24 19:28:12 +0530

    add hte No argument constructor annotation of lombok to get the null parameterize constructor for the class

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
@@ -2,9 +2,11 @@ package com.railway.scheduling.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Station implements Comparable<Station>{
     private String name;
     private int g; //cost from start
