93fa13e73144f047a21896dcf61111cbfafcb9be
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-24 19:19:56 +0530

    add the parameter which is cost from start

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/Station.java
@@ -2,7 +2,7 @@ package com.railway.scheduling.DTO;
 
 public class Station implements Comparable<Station>{
     private String name;
-
+    private int g; //cost from start
     @Override
     public int compareTo(Station o) {
         return 0;
