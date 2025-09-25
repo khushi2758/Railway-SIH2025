50685d70ada5dc7f91d0e456953f56b97d47b4f7
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-25 11:24:22 +0530

    created a Map data structure where I store the heuristic value

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
@@ -1,4 +1,9 @@
 package com.railway.scheduling.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AstarAlgorithm {
+    private static final Map<String,Integer> heuristic = new HashMap<>();
+
 }
