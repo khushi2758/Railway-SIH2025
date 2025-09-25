4dfb2f0afbab6984b12265bcf0f7a28baab1c404
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-25 11:37:36 +0530

    create a Set data structure where I store the close set

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
@@ -3,15 +3,13 @@ package com.railway.scheduling.Service;
 import com.railway.scheduling.DTO.Edge;
 import com.railway.scheduling.DTO.Station;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class AstarAlgorithm {
     private final Map<String,Integer> heuristic = new HashMap<>();
     public List<String> aStar(Map<String,List<Edge>> graph,String start,String goal){
         PriorityQueue<Station> openList = new PriorityQueue<>();
         Map<String,String> gScore = new HashMap<>();
+        Set<String> closeSet = new HashSet<>();
     }
 }
