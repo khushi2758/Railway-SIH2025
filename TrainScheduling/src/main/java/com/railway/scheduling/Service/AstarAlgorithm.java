ad71707aee9ada9bba91c5ef61f652925a0f0453
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-25 13:02:35 +0530

    complete the function

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
@@ -6,12 +6,12 @@ import com.railway.scheduling.DTO.Station;
 import java.util.*;
 
 public class AstarAlgorithm {
-    private final Map<String,Integer> heuristic = new HashMap<>();
+    private final Map<String,Float> heuristic = new HashMap<>();
     public List<String> aStar(Map<String,List<Edge>> graph,String start,String goal){
         PriorityQueue<Station> openList = new PriorityQueue<>();
-        Map<String,Integer> gScore = new HashMap<>();
+        Map<String,Float> gScore = new HashMap<>();
         Set<String> closeSet = new HashSet<>();
-        gScore.put(start,0);
+        gScore.put(start,0.0F);
         openList.add(new Station(start,0,heuristic.get(start),null));
         while (!openList.isEmpty()){
             Station current = openList.poll();
@@ -20,9 +20,16 @@ public class AstarAlgorithm {
             }
             closeSet.add(current.getName());
             for(Edge edge: graph.getOrDefault(current.getName(),new ArrayList<>())){
-//                if(closeSet.contains(edge.))
+                if(closeSet.contains(edge.getTarget())) continue;
                 float tentativeG = current.getG() + edge.getWeight();
+                if(tentativeG< gScore.getOrDefault(edge.getTarget(),Float.MAX_VALUE)){
+                    gScore.put(edge.getTarget(),tentativeG);
+                    float f = tentativeG + heuristic.getOrDefault(edge.getTarget(),0.0F);
+                    Station neighbor = new Station(edge.getTarget(), tentativeG,f,current);
+                    openList.add(neighbor);
+                }
             }
         }
+        return Collections.emptyList();
     }
 }
