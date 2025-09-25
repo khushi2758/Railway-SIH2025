5be66e8a9b907357228a1decce3af926c57344a4
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-25 11:27:55 +0530

    create a method where I perform the A* algorithm that return the list of the shorted path stations and take input of the all stations, starting station & destination station

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
@@ -1,8 +1,14 @@
 package com.railway.scheduling.Service;
 
+import com.railway.scheduling.DTO.Edge;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AstarAlgorithm {
     private final Map<String,Integer> heuristic = new HashMap<>();
+    public List<String> aStar(Map<String,List<Edge>> graph,String start,String goal){
+
+    }
 }
