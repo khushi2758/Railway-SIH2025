a061e3f2db1dba51ace3673209c6fe0d84f8b509
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-25 11:32:53 +0530

    create the priority queue for the compare the stations(Node)

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/AstarAlgorithm.java
@@ -1,14 +1,16 @@
 package com.railway.scheduling.Service;
 
 import com.railway.scheduling.DTO.Edge;
+import com.railway.scheduling.DTO.Station;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class AstarAlgorithm {
     private final Map<String,Integer> heuristic = new HashMap<>();
     public List<String> aStar(Map<String,List<Edge>> graph,String start,String goal){
-
+        PriorityQueue<Station> openList = new PriorityQueue<>();
     }
 }
