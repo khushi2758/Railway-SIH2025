3dfd180f16c3b0d2a23dc18d81b0e85b1102fb45
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-26 23:13:43 +0530

    wrap up whole thing with try-catch block

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackDataInsertion.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackDataInsertion.java
@@ -10,11 +10,10 @@ import java.util.List;
 public class TrackDataInsertion {
     private TrackRepository trackRepository;
     public String insertTrackData(long trackId, float maximumSpeed){
-        Track track = new Track();
-        track.setTrackID(trackId);
-        track.setMaximumSpeed(maximumSpeed);
         try{
-
+            Track track = new Track();
+            track.setTrackID(trackId);
+            track.setMaximumSpeed(maximumSpeed);
         }
     }
 }
