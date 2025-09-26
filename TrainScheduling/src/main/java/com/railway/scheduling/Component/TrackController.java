eb7cb49e2b076b68adb4d6d31b605328d1ab84bf
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 01:00:48 +0530

    create the function that return the response entity of map data structure and take data as request param

--- TrainScheduling/src/main/java/com/railway/scheduling/Component/TrackController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Component/TrackController.java
@@ -2,12 +2,19 @@ package com.railway.scheduling.Component;
 
 import com.railway.scheduling.Service.TrackDataInsertion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/track")
 public class TrackController {
     @Autowired
     private TrackDataInsertion trackDataInsertion;
+    public ResponseEntity<Map<String,String>> TrackRegistration(@RequestParam String id, String maxSpeed){
+
+    }
 }
