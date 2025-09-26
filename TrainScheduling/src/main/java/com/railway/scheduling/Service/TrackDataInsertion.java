bb00faa46c64ca29e3697b52f5855c1ef3c85914
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-26 22:54:17 +0530

    create the instance of the track repository

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackDataInsertion.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackDataInsertion.java
@@ -1,7 +1,9 @@
 package com.railway.scheduling.Service;
 
+import com.railway.scheduling.Repository.TrackRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrackDataInsertion {
+    private TrackRepository trackRepository;
 }
