11ec4f981542dd555a897a56f83d6cbe1e3d36af
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 00:51:53 +0530

    autowired it so automatically dependency injection

--- TrainScheduling/src/main/java/com/railway/scheduling/Component/TrackController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Component/TrackController.java
@@ -1,11 +1,13 @@
 package com.railway.scheduling.Component;
 
 import com.railway.scheduling.Service.TrackDataInsertion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/track")
 public class TrackController {
+    @Autowired
     private TrackDataInsertion trackDataInsertion;
 }
