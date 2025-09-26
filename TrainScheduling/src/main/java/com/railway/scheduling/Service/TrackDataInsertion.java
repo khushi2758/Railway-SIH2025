f10556527df2414f35c451bffd697a361b691e46
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-26 23:03:03 +0530

    create a function that interact with repository layer and store the track data in the database

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackDataInsertion.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackDataInsertion.java
@@ -1,9 +1,15 @@
 package com.railway.scheduling.Service;
 
+import com.railway.scheduling.Entity.Track;
 import com.railway.scheduling.Repository.TrackRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TrackDataInsertion {
     private TrackRepository trackRepository;
+    public String insertTrackData(long trackId, float maximumSpeed){
+
+    }
 }
