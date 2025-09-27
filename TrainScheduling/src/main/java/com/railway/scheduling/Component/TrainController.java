285622b6bf330ae073c16efa9a00ad05e649ff8e
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 23:36:12 +0530

    add the annotation of request mapping

--- TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
@@ -1,7 +1,9 @@
 package com.railway.scheduling.Component;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/train")
 public class TrainController {
 }
