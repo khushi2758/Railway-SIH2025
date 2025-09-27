e77332b122deb01e6c50ccad092a75abacddc90e
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 20:57:35 +0530

    add the private instance of track service

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/GraphConstructor.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/GraphConstructor.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class GraphConstructor {
+    private TrackService trackService;
     public List<String> SortestPathUsingAstar(){
         Map<String,List<Edge>> graph = new HashMap<>();
     }
