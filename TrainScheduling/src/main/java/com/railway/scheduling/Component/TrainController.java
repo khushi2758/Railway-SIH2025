ab9dce7c45f327758be43e4e09767e1f994c6154
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 23:55:02 +0530

    create a public function that take train dto as a parameter and return response entity of Map and work as register new train

--- TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
@@ -1,13 +1,21 @@
 package com.railway.scheduling.Component;
 
+import com.railway.scheduling.DTO.TrainDTO;
 import com.railway.scheduling.Service.TrainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/train")
 public class TrainController {
     @Autowired
     private TrainService trainService;
+    public ResponseEntity<Map<String,String>> RegisterTrain(@RequestBody TrainDTO trainDTO){
+
+    }
 }
