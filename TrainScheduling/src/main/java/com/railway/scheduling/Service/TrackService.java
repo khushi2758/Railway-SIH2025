75f3f2e04a746715d994b6c02eefd1591906e88c
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 15:48:52 +0530

    change the class name

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/TrackService.java
@@ -5,10 +5,8 @@ import com.railway.scheduling.Repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class TrackDataInsertion {
+public class TrackService {
     @Autowired
     private TrackRepository trackRepository;
     public String insertTrackData(long trackId, float maximumSpeed){
