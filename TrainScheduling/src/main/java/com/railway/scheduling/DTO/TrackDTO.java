a6886af307644764366e726e74edcc176f648e68
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 20:32:09 +0530

    change the data members data type

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrackDTO.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrackDTO.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TrackDTO {
-    private Long trackID;
-    private Float maximumSpeed;
+    private String trackID;
+    private String maximumSpeed;
     private String SourceStation;
     private String DestinationStation;
 }
