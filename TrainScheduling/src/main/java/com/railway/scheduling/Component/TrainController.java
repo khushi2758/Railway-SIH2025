711cb1ce6a78ac87d9319de40e05ba3866861b86
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-28 00:01:41 +0530

    change the data type from string to Time

--- TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
@@ -1,6 +1,7 @@
 package com.railway.scheduling.Component;
 
 import com.railway.scheduling.DTO.TrainDTO;
+import com.railway.scheduling.Entity.Train;
 import com.railway.scheduling.Service.TrainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class TrainController {
     public ResponseEntity<Map<String,String>> RegisterTrain(@RequestBody TrainDTO trainDTO){
         Long trainId = Long.parseLong(trainDTO.getTrainId());
         Time departureTime = Time.valueOf(trainDTO.getDepartureTime());
-        Time startBlockTimeInterval = trainDTO.getStartBlockTimeInterval();
+        Time startBlockTimeInterval = Time.valueOf(trainDTO.getStartBlockTimeInterval());
         Time endTimeCoverBlock = trainDTO.getEndTimeCoverBlock();
         Float trainLength = trainDTO.getTrainLength();
         Float trainSpeed = trainDTO.getTrainSpeed();
