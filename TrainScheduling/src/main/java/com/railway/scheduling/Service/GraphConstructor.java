95f12e61a70704621233a23abc181fc8043cc905
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 20:56:01 +0530

    create the map data structure that represent the graph

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/GraphConstructor.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/GraphConstructor.java
@@ -1,9 +1,13 @@
 package com.railway.scheduling.Service;
 
+import com.railway.scheduling.DTO.Edge;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GraphConstructor {
     public List<String> SortestPathUsingAstar(){
-
+        Map<String,List<Edge>> graph = new HashMap<>();
     }
 }
