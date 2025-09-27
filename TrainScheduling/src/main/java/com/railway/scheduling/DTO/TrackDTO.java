383d37b55fb014f981f67eb0ae354d6e291999ba
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 20:24:01 +0530

    add the private data members of the DTO layer

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrackDTO.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrackDTO.java
@@ -1,4 +1,8 @@
 package com.railway.scheduling.DTO;
 
 public class TrackDTO {
+    private Long trackID;
+    private Float maximumSpeed;
+    private String SourceStation;
+    private String DestinationStation;
 }
