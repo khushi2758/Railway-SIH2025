67808e7d8aed57a71305c4fbc2065e35dc971578
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 22:28:29 +0530

    create a new function that register a train in database with required attributes

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/TrainService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/TrainService.java
@@ -1,9 +1,16 @@
 package com.railway.scheduling.Service;
 
+import com.railway.scheduling.Entity.Train;
 import com.railway.scheduling.Repository.TrainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Time;
+import java.util.List;
 
 public class TrainService {
     @Autowired
     private TrainRepository trainRepository;
+    public String TrainRegistration(Long trainId, Time departureTime, Time starBlockTimeInterval, Time endTimeCoverBlock, Float trainLength, Float trainSpeed, Integer priority, Float deviationPenalty, Boolean isOnList, Time waitingTime){
+
+    }
 }
