8101c2a5481acf15129e57c99840a7cb7cec2450
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 20:58:00 +0530

    autowired it for automatically dependency injection

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/GraphConstructor.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/GraphConstructor.java
@@ -1,12 +1,14 @@
 package com.railway.scheduling.Service;
 
 import com.railway.scheduling.DTO.Edge;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GraphConstructor {
+    @Autowired
     private TrackService trackService;
     public List<String> SortestPathUsingAstar(){
         Map<String,List<Edge>> graph = new HashMap<>();
