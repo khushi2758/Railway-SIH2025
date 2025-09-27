524391fc64e06d5a9ef99cd7f6b51f0e5d1c56bf
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 23:41:55 +0530

    add the private data member for the DTO layer

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrainDTO.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrainDTO.java
@@ -1,4 +1,14 @@
 package com.railway.scheduling.DTO;
 
 public class TrainDTO {
+    private Long trainId;
+    private String departureTime;
+    private String startBlockTimeInterval;
+    private String endTimeCoverBlock;
+    private String trainLength;
+    private String trainSpeed;
+    private String priority;
+    private String deviationPenalty;
+    private String  isOnList;
+    private String waitingTime;
 }
