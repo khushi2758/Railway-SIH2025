d6dbc002aa232d7ba5edeb05fcd931c63f80a78b
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 22:17:59 +0530

    autowired it for automatically dependency injection

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/TrainService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/TrainService.java
@@ -1,7 +1,9 @@
 package com.railway.scheduling.Service;
 
 import com.railway.scheduling.Repository.TrainRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TrainService {
+    @Autowired
     private TrainRepository trainRepository;
 }
