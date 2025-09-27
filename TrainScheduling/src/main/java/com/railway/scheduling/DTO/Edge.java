4a4aff6490a5009ec4ca51ee1be37e9ae02d0fd2
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 22:12:49 +0530

    remove the unwanted attribute of the constructor

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/Edge.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/Edge.java
@@ -12,7 +12,7 @@ import java.sql.Time;
 public class Edge {
     private Track track;
     private Float weight;
-    public Edge(Track track, int priority, int deviationPenalty, boolean isOnList, float trackLength, Time waitingTime){
+    public Edge(Track track, int priority, int deviationPenalty, boolean isOnList, Time waitingTime){
         this.track = track;
         this.weight = Formulas.weight(priority,deviationPenalty,isOnList,track.getTrackLength(),track.getMaximumSpeed(),waitingTime);
     }
