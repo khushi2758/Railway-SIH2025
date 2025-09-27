3d88ab1e7d2b3153bc4e7735e9e9ed9a9cf4f17c
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 22:36:17 +0530

    return the success message if data successfully register

--- TrainScheduling/src/main/java/com/railway/scheduling/Service/TrainService.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Service/TrainService.java
@@ -24,6 +24,7 @@ public class TrainService {
             train.setIsOnList(isOnList);
             train.setWaitingTime(waitingTime);
             trainRepository.save(train);
+            return "OK"
         }
     }
 }
