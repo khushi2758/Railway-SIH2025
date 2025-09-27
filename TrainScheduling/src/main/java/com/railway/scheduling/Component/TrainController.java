6e40432769990d2386ee72bd302306a90a9e8b72
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 23:36:53 +0530

    create the private instance of train service

--- TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Component/TrainController.java
@@ -1,9 +1,11 @@
 package com.railway.scheduling.Component;
 
+import com.railway.scheduling.Service.TrainService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/train")
 public class TrainController {
+    private TrainService trainService;
 }
