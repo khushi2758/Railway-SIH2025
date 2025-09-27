19bc770a0a2fd5d6c1603cce5c6b89dfe44befc9
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 23:43:20 +0530

    add no argument constructor for the Train DTO layer

--- TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrainDTO.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/DTO/TrainDTO.java
@@ -2,9 +2,11 @@ package com.railway.scheduling.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TrainDTO {
     private Long trainId;
     private String departureTime;
