4ec8607fe53aed5d0950114853ec411b605dd225
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-09-27 20:28:37 +0530

    add the request body as the parameter

--- TrainScheduling/src/main/java/com/railway/scheduling/Component/TrackController.java
+++ TrainScheduling/src/main/java/com/railway/scheduling/Component/TrackController.java
@@ -1,10 +1,11 @@
 package com.railway.scheduling.Component;
 
+import com.railway.scheduling.DTO.TrackDTO;
 import com.railway.scheduling.Service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -14,7 +15,7 @@ import java.util.Map;
 public class TrackController {
     @Autowired
     private TrackService trackDataInsertion;
-    public ResponseEntity<Map<String,String>> TrackRegistration(@RequestParam String id, String maxSpeed){
+    public ResponseEntity<Map<String,String>> TrackRegistration(@RequestBody TrackDTO trackDTO){
         long trackId = Long.parseLong(id);
         float maxSpeedAllowed = Float.parseFloat(maxSpeed);
         String res = trackDataInsertion.insertTrackData(trackId,maxSpeedAllowed);
